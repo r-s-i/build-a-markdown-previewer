28cc3f9eccdc5be4c7bf93ba0970a6010637e22c
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-01-25 21:19:22 +0100

    Changed multiline element to be multiline.

--- src/MarkdownPreviewer.js
+++ src/MarkdownPreviewer.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 
 function MarkdownPreviewer() {
-  const defaultMarkdown = `# One hash\n## Two hashes\n[A link to my super cool website](https://www.robiniversen.com)\n- A list item \n\n**VERY IMPORTANT INFO**\n\nHeres some code, \`<div></div>\`, between 2 backticks.\n\n \`\`\`test\`\`\`\n\n> Block Quotes!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-
+  const defaultMarkdown = `# One hash\n## Two hashes\n[A link to my super cool website](https://www.robiniversen.com)\n- A list item \n\n**VERY IMPORTANT INFO**\n\nHeres some code, \`<div></div>\`, between 2 backticks.\n\n \`\`\`\nfunction example(param1, param2) {
+    return param1 + param2;
+  } \n\`\`\`\n\n> Block Quotes!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 `;
 
   const [markdown, setMarkdown] = useState(defaultMarkdown);
@@ -20,9 +21,12 @@ function MarkdownPreviewer() {
         value={markdown} 
         onChange={handleChange} 
       />
-      <div id="preview">
-      <ReactMarkdown children={markdown} 
-      /> 
+      <div>
+        <div id="preview">
+        <ReactMarkdown
+          children={markdown}
+        />
+        </div>
       </div>
     </div>
   );
