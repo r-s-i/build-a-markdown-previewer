5e10ce33fcc3a7a43d98ff6192b0e791f06e9e85
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-01-26 01:34:03 +0100

    Added h1.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import {MarkdownPreviewer} from './MarkdownPreviewer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Markdown Previewer</h1>
       <MarkdownPreviewer/>
     </div>
   );
