3805075e29024c1f405813888dfd506df7c0712d
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-01-26 01:16:52 +0100

    Changed stand-in img.

--- src/MarkdownPreviewer.js
+++ src/MarkdownPreviewer.js
@@ -5,7 +5,7 @@ import ReactMarkdown from 'react-markdown';
 function MarkdownPreviewer() {
   const defaultMarkdown = `# One hash\n## Two hashes\n[A link to my super cool website](https://www.robiniversen.com)\n- A list item \n\n**VERY IMPORTANT INFO**\n\nHeres some code, \`<div></div>\`, between 2 backticks.\n\n \`\`\`\nfunction example(param1, param2) {
     return param1 + param2;
-  } \n\`\`\`\n\n> Block Quotes!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  } \n\`\`\`\n\n> Block Quotes!\n\n![freeCodeCamp Logo](https://robiniversen.com/stand-in-img.webp)
 `;
 
   const [markdown, setMarkdown] = useState(defaultMarkdown);
